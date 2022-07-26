b6c74ed9a70551296b2c77ff9ac91138e89861e9
manyblake <manyblake@gmail.com>
2022-07-26 17:02:21 +0200

    bonus2 (add circles)

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const slidesWrapper = document.querySelector(`.carousel__wrapper`);
 const nextArrow = document.querySelector(`.carousel__arrow--next`);
 const prevArrow = document.querySelector(`.carousel__arrow--prev`);
+const pagination = document.querySelector(`.carousel__pagination`);
 
 const slides = [
   `./img/01.jpg`,
@@ -11,26 +12,33 @@ const slides = [
 ];
 
 const slidesElements = [];
+const buttonsElements = [];
 let currentIndex = 0;
 
 for (let i = 0; i < slides.length; i++) {
   const li = document.createElement("li");
   const src = slides[i];
   const img = document.createElement("img");
+  const button = document.createElement("button");
   img.src = src;
   li.appendChild(img);
   li.className = `carousel__slide`;
   slidesWrapper.appendChild(li);
   slidesElements.push(li);
+  pagination.appendChild(button);
+  buttonsElements.push(button);
 
   if (i === currentIndex) {
     li.classList.add(`carousel-slide--active`);
+    button.classList.add(`active`);
   }
 }
 
 nextArrow.addEventListener("click", function () {
   const slideActive = slidesElements[currentIndex];
+  const buttonActive = buttonsElements[currentIndex];
   slideActive.classList.remove(`carousel-slide--active`);
+  buttonActive.classList.remove(`active`);
 
   if (currentIndex < slidesElements.length - 1) {
     currentIndex++;
@@ -39,12 +47,16 @@ nextArrow.addEventListener("click", function () {
   }
 
   const nextSlide = slidesElements[currentIndex];
+  const nextButton = buttonsElements[currentIndex];
   nextSlide.classList.add(`carousel-slide--active`);
+  nextButton.classList.add(`active`);
 });
 
 prevArrow.addEventListener("click", function () {
   const slideActive = slidesElements[currentIndex];
+  const buttonActive = buttonsElements[currentIndex];
   slideActive.classList.remove(`carousel-slide--active`);
+  buttonActive.classList.remove(`active`);
 
   if (currentIndex > 0) {
     currentIndex--;
@@ -53,5 +65,7 @@ prevArrow.addEventListener("click", function () {
   }
 
   const nextSlide = slidesElements[currentIndex];
+  const nextButton = buttonsElements[currentIndex];
   nextSlide.classList.add(`carousel-slide--active`);
+  nextButton.classList.add(`active`);
 });
